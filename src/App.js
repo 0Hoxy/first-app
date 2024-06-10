899853b29af57cfd3bcb1853204b8c16dcacf645
0Hoxy <tinyfinger@naver.com>
2024-06-10 16:38:06 +0900

    Study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css';
+
 
 function App() {
+  //자바스트립트와 태그를 같이 쓰는 문법을 JSX라고 한다(리액트 문법)
+
+  const [pets, setPets] = useState([]);
+  //useEffect를 사용해 데이터를 localStorage에 저장해서 불러오기
+  //처음 한번만 실행됨
+  useEffect(() => {
+    if (localStorage.getItem("PetData")) {
+      setPets(JSON.parse(localStorage.getItem("PetData")));
+    }
+  }, []);
+  //pets가 수정될때마다 실행됨
+  useEffect(() => {
+    localStorage.setItem("PetData", JSON.stringify(pets));
+  }, [pets]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <OurHeader />
+      <LikeArea />
+      <TimeArea />
+      <AddPetForm setPets={setPets} />
+
+      <ul>
+        {pets.map(function (pet) {
+          return <Pet name={pet.name} species={pet.species} age={pet.age} key={pet.id} setPets={setPets} id={pet.id} />;
+        })}
+      </ul>
+      <Footer cr="부산IT교육센터" />
+    </>
   );
 }
 
+function OurHeader() {
+  return <h1 className='special'>처음 앱</h1>;
+}
+
+function TimeArea() {
+  const [time, setTime] = useState(new Date().toLocaleString());
+
+  setTimeout(() => {
+    setTime(new Date().toLocaleString());
+  }, 1000);
+
+  return <p>현재 시간 : {time}</p>;
+}
+
+function Footer(props) {
+  return <small>Copyright: {props.cr}</small>;
+}
+
+function Pet(props) {
+  function handleDelete() {
+    props.setPets(prev => prev.filter(pet => pet.id !== props.id));
+  }
+  return (
+    <li>{props.name}은(는) {props.species}이고 {props.age}살 이다.
+      <button onClick={handleDelete}>삭제</button>
+    </li>
+  );
+}
+
+function LikeArea() {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function increaseLike() {
+    setLikeCount(prev => prev + 1);
+  }
+
+  function decreaseLike() {
+    setLikeCount(prev => prev - 1);
+  }
+
+  return (
+    <>
+      <button onClick={increaseLike}>추천하기</button>
+      <button onClick={decreaseLike}>비추하기</button>
+      <h2>이 페이지를 {likeCount} 번 추천 했습니다.</h2>
+    </>
+  );
+}
+
+function AddPetForm(props) {
+  const [name, setName] = useState("");
+  const [species, setSpecies] = useState("");
+  const [age, setAge] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.setPets(prev => prev.concat({ name: name, species: species, age: age, id: new Date() }));
+    setName("");
+    setSpecies("");
+    setAge("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>새 PET 을 추가하기</legend>
+        <input value={name} onChange={e => setName(e.target.value)} placeholder='이름' />
+        <input value={species} onChange={e => setSpecies(e.target.value)} placeholder='종류' />
+        <input value={age} onChange={e => setAge(e.target.value)} placeholder='나이' />
+        <button>펫 추가</button>
+      </fieldset>
+    </form>
+  );
+}
+
+
+
+
+
+
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(function () {
+  root.render(React.createElement(App));
+}, 1000);
+
+// function App() {
+//   return React.createElement("div", null, [
+//     React.createElement("h1", null, "처음 앱"),
+//     React.createElement("p", null, `현재 시간 : ${new Date().toLocaleString()}.`),
+//     React.createElement("small", null, "Copyright Footer Text")
+//     //type, (태그 이름 문자열 | 리액트 컴포넌트 | React.Fragment)
+//     //[props], (리액트 컴포넌트에 넣어주는 데이터 객체)
+//     //[ ... children]  (자식으로 넣어주는 요소들)
+//   ]);
+// }
